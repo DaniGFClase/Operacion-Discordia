6a1da3bf1235e3387e021e49943900b0fe12f9e2
Adrian Rodriguez <adrianuhu@gmail.com>
2020-11-07 20:07:08 +0100

    Update zz_TEST.php

--- zz_TEST.php
+++ zz_TEST.php
@@ -19,11 +19,8 @@ function load_config($name, $schema){
 	return $result;
 }
 
-function send_Message(){
+function send_Message($myUser, $toUser, $message){
 
-    $userA = 3;
-    $userB = 4;
-    $userC = 5;
 
 	$res = load_config(dirname(__FILE__)."/configuration.xml", dirname(__FILE__)."/configuration.xsd");
 	$db = new PDO($res[0], $res[1], $res[2]);
@@ -38,17 +35,24 @@ function send_Message(){
     
 
     if (!$r) {
-        createTable ($userA, $userB);
+        createTable ($myUser, $toUser);
     }
 
+    $cod_room = "";
     foreach($result as $re){	
 
-       if ($re['users']==($userA.",".$userB) || $re['users']==($userB.",".$userA)) {
-           echo $re['cod_room'];
+       if ($re['users']==($myUser.",".$toUser) || $re['users']==($toUser.",".$myUser)) {
+        $cod_room = $re['cod_room'];
        }else {
-        createTable($userA, $userB);
+        createTable($myUser, $toUser);
        }
     }
+
+    
+
+    $ins = "INSERT INTO `message` (`cod_message`, `cod_user`, `text_message`, `date_message`, `cod_room`) VALUES (NULL, '$myUser', '$message', current_timestamp(), '$cod_room')";
+	
+    $result = $db->query($ins);
 
 }
 
@@ -63,4 +67,4 @@ function createTable ($userA, $userB){
     $resul = $db->query($ins);
 }
 
-send_Message();
+send_Message(17, 4, "te exploto payaso");
