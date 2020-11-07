b83d0e948e89e13d9be6fd95d72763213a39b0c8
Adrian Rodriguez <adrianuhu@gmail.com>
2020-11-07 20:16:51 +0100

    Update db.php

--- db.php
+++ db.php
@@ -68,8 +68,52 @@ function register_user($name, $surname, $nick, $email, $password, $gender){
 
 }
 
-function sendMenssage (){
+function send_Message($myUser, $toUser, $message){
+
+
+	$res = load_config(dirname(__FILE__)."/configuration.xml", dirname(__FILE__)."/configuration.xsd");
+	$db = new PDO($res[0], $res[1], $res[2]);
+	$ins = "select count(*) as count, group_concat(cod_user separator ',') as users, cod_room
+    from user_room 
+    group by cod_room
+    having count like 2";
 	
+    $resul = $db->query($ins);
+    $result = $db->query($ins);
+    $r = $resul->fetch();
+    
+
+    if (!$r) {
+        createTable ($myUser, $toUser);
+    }
+
+    $cod_room = "";
+    foreach($result as $re){	
+
+       if ($re['users']==($myUser.",".$toUser) || $re['users']==($toUser.",".$myUser)) {
+        $cod_room = $re['cod_room'];
+       }else {
+        createTable($myUser, $toUser);
+       }
+    }
+
+    
+
+    $ins = "INSERT INTO `message` (`cod_message`, `cod_user`, `text_message`, `date_message`, `cod_room`) VALUES (NULL, '$myUser', '$message', current_timestamp(), '$cod_room')";
+	
+    $result = $db->query($ins);
+
+}
+
+function createTable ($userA, $userB){
+    $res = load_config(dirname(__FILE__)."/configuration.xml", dirname(__FILE__)."/configuration.xsd");
+    $db = new PDO($res[0], $res[1], $res[2]);
+    
+    $ins = " INSERT INTO `room` (`cod_room`, `name_room`) VALUES ('$userA-$userB', '')"; 
+    $resul = $db->query($ins);
+
+    $ins = " INSERT INTO `user_room` (`cod_user`, `cod_room`) VALUES ('$userA', '$userA-$userB'), ('$userB', '$userA-$userB')"; 
+    $resul = $db->query($ins);
 }
 
 
