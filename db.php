dd849574a76b2f5842c2f4538bcbf075bda2f71f
Adrian Rodriguez <adrianuhu@gmail.com>
2020-11-07 16:41:02 +0100

    Update db.php

--- db.php
+++ db.php
@@ -23,24 +23,25 @@ function load_config($name, $schema){
 function check_user($name, $password){
 	$res = load_config(dirname(__FILE__)."/configuration.xml", dirname(__FILE__)."/configuration.xsd");
 	$db = new PDO($res[0], $res[1], $res[2]);
-	$ins = "select cod_User, mail from restaurants where (nick = '$name' or mail ='$name') 
+	$ins = "select cod_User, mail from user where (nick = '$name' or mail ='$name') 
 			and password_hash = '$password'";
 	
-	$hash = "select password_hash from restaurants where (nick = '$name' or mail ='$name') 
-			and password_hash = '$password'";
-	$hash_resul = $db->query($ins);
-	echo ($name);
+	$hash = "select password_hash from user where (nick = '$name' or mail ='$name')";
+	$hash_resul = $db->query($hash);
+
+
+	if ($hash_resul->rowCount() === 1) {    
+		$pass = $hash_resul->fetch();
+		echo($pass['password_hash']);
+	}
 	
-			if (password_verify('$password', $hash_resul)) {
-				$resul = $db->query($ins);	
-				if($resul->rowCount() === 1){		
-					return $resul->fetch();		
-				}else{
-					return FALSE;
-				}
-			} else {
-				return FALSE;
-			}
+		
+	$resul = $db->query($ins);	
+	if($resul->rowCount() === 1 && password_verify($password, $pass)){		
+		return $resul->fetch();		
+	}else{
+	return FALSE;
+	}		
 
 }
 function load_categories(){
