7211896c0ee1b9018e726bb81e8f12f26bc52e44
Adrian Rodriguez <adrianuhu@gmail.com>
2020-11-22 15:27:52 +0100

    aaa

--- pages/chat_AJAX.php
+++ pages/chat_AJAX.php
@@ -60,9 +60,9 @@
                 <input id="avatar_chat" type="hidden" value = "<?php echo $avatar_chat?>">
                 <input id="name_chat" type="hidden" value = "<?php echo $name_chat?>">
 
-			    <input type = "submit" class="sendBtn">
-                <label for="file-upload" class="sendBtn"><b>Choose file</b></label>
-                <input id="file-upload" type="file" name="myfile" value="<?php echo $currentData['photo'] ?>">
+			    
+                <label for="file-upload" class="sendBtn">Choose file</label>
+                <input id="file-upload" type="file" class="sendBtn" name="myfile" value="">
 
 			    <input type = "submit" class="sendBtn" value="Send">
 
