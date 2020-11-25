7deb11760798f3d2dac849a81c7a3728ec4d593f
DaniGFClase <60668304+DaniGFClase@users.noreply.github.com>
2020-11-25 20:21:30 +0100

    a

--- pages/upload_files.php
+++ pages/upload_files.php
@@ -13,4 +13,9 @@ if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) {
   echo 'Failure'; 
 }
 
+
+  send_chat_Message($_SESSION['user']['cod_user'], $_POST['codRoom'], $filename);
+  setNotView($_POST['codRoom'], $_SESSION['user']['cod_user']);
+        
+
 ?>
